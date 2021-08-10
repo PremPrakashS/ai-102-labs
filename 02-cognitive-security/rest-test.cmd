c5131ced847d6d4d1dd721f0a2533d7416290b88
PremPrakashS <premp7420@gmail.com>
2021-08-10 10:13:55 +0000

    first commit

--- 02-cognitive-security/rest-test.cmd
+++ 02-cognitive-security/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "<yourEndpoint>/text/analytics/v3.0/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <yourKey>" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
+curl -X POST "https://azure-ai-100-labs-cogser03.cognitiveservices.azure.com/text/analytics/v3.0/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 08b1abf764514178a36ef724c5d13724" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
